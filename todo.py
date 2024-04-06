63396f7ae6846dad2667da44a7040b2c2114f2ac
shibino-developer <shibino.developer@gmail.com>
2024-04-06 10:06:55 +0530

    Implemented File saving

--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
 from task import Task
+import json
 
 class TodoList:
     def __init__(self):
@@ -12,3 +13,16 @@ class TodoList:
 
     def update_task(self, index, new_task):
         self.tasks[index] = new_task
+
+    def save_tasks(self, filename):
+        with open(filename, 'w') as file:
+            json.dump([task.__dict__ for task in self.tasks], file)
+    
+    def load_tasks(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                tasks_data = json.load(file)
+                self.tasks = [Task(**task_data) for task_data in tasks_data]
+        except FileNotFoundError:
+            # If the file doesn't exist, initialize an empty list of tasks
+            self.tasks = []
